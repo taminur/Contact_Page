ad8ada4a1612cae0e96bce23f524bc42f3f565b8
taminur <taminur@live.com>
2023-07-21 21:28:42 +0600

    This is the basic connection between external contact page and flask server

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-import os
+import os # used to access environment variable
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS # to allow access methods from external domain
+
+app = Flask(__name__)
+origin="./static/contact.html"
+CORS(app, origins=origin)
 
 def send_email(sender_email, sender_password, recipient_email, subject, message):
     try:
@@ -30,16 +37,42 @@ def send_email(sender_email, sender_password, recipient_email, subject, message)
         server.quit()
 
         print('Email sent successfully!')
+        return True
 
     except Exception as e:
         print(f'Error sending email: {str(e)}')
+        return False
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api/send", methods=["GET","POST"])
+def send():
+    if request.method == "GET":
+        return render_template("success.html")
+    elif request.method == "POST":
+        print('You post data')
+        sender_email = os.environ.get('ZOHO_USER')  # ZOHO_USER is the environment variable which contains actual email
+        sender_password = os.environ.get('ZOHO_APP_PWD')  # ZOHO_APP_PWD is also the environment variable
+        
+        recipient_email = sender_email.replace('zoho','live')
+        name = request.form.get('name')
+        email = request.form.get('email')
+        subject = request.form.get('sub')
+        message = request.form.get('msg')
+        total_msg = f'{name} having email {email} sent message through contact page\n\n\
+                    ---------\n\
+                    Subject: {subject}\n\
+                    ----------\n\
+                    {message}'
+
+        print(name, email, subject, message)
+        # send_email(sender_email, sender_password, recipient_email, subject, total_msg)
+        # return render_template("success.html")
+        return redirect(url_for('static', filename='contact.html'))
+    else:
+        return render_template("failure.html")
 
 if __name__ == '__main__':
-    sender_email = os.environ.get('ZOHO_USER')  # ZOHO_USER is the environment variable which contains actual email
-    sender_password = os.environ.get('ZOHO_APP_PWD')  # ZOHO_APP_PWD is also the environment variable
-    
-    recipient_email = sender_email.replace('zoho', 'live')  # Replace with the recipient's email address
-    subject = 'Test Email from Zoho Mail using app with environment variable'
-    message = 'This is a test email sent from a Python script using Zoho Mail.'
-
-    send_email(sender_email, sender_password, recipient_email, subject, message)
+    app.run()
